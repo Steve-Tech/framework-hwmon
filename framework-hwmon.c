e90e274b09b8c4dc8d65e9e4b2449f1fc6d386b3
Stephen Horvath <s.horvath@outlook.com.au>
2024-02-21 08:58:15 +1000

    Add some error checking

--- framework-hwmon.c
+++ framework-hwmon.c
@@ -14,6 +14,9 @@ static ssize_t read_fan_speeds(u8 idx, u16 *val)
 {
 	struct cros_ec_device *ec;
 
+	if (!ec_device)
+		return -ENODEV;
+
 	ec = dev_get_drvdata(ec_device);
 
 	u8 offset = EC_MEMMAP_FAN + 2 * idx;
@@ -35,10 +38,16 @@ static ssize_t show_fan_speed(struct device *dev,
 	return sprintf(buf, "%d\n", val);
 }
 
-static SENSOR_DEVICE_ATTR(fan1_input, 0444, show_fan_speed, NULL, 0);
+// clang-format off
+static SENSOR_DEVICE_ATTR(fan1_input, 0444, show_fan_speed, NULL, 0); // Fan Reading
+// static SENSOR_DEVICE_ATTR(fan1_target, 0644, show_fan_target, set_fan_target, 0); // Target RPM
+// static SENSOR_DEVICE_ATTR(pwm1_enable, 0644, show_pwm_enable, set_pwm_enable, 0); // Set Fan Control Mode
+// static SENSOR_DEVICE_ATTR(pwm1, 0644, show_pwm, set_pwm, 0); // Set Fan Speed
+
 static SENSOR_DEVICE_ATTR(fan2_input, 0444, show_fan_speed, NULL, 1);
 static SENSOR_DEVICE_ATTR(fan3_input, 0444, show_fan_speed, NULL, 2);
 static SENSOR_DEVICE_ATTR(fan4_input, 0444, show_fan_speed, NULL, 3);
+// clang-format on
 
 static struct attribute *fw_hwmon_attrs[EC_FAN_SPEED_ENTRIES + 1] = {
 	&sensor_dev_attr_fan1_input.dev_attr.attr,
@@ -65,6 +74,8 @@ static int device_match_cros_ec(struct device *dev, const void *foo)
 
 static int __init fw_hwmon_init(void)
 {
+    struct cros_ec_device *ec;
+
 	ec_device = bus_find_device(&platform_bus_type, NULL, NULL,
 				    device_match_cros_ec);
 	if (!ec_device) {
@@ -72,6 +83,12 @@ static int __init fw_hwmon_init(void)
 		return -EINVAL;
 	}
 	ec_device = ec_device->parent;
+	ec = dev_get_drvdata(ec_device);
+
+	if (!ec->cmd_readmem) {
+		printk(KERN_WARNING "framework-hwmon: EC not supported.\n");
+		return -EINVAL;
+	}
 
 	// Count the number of fans
 	for (size_t i = 0; i < EC_FAN_SPEED_ENTRIES; i++) {
