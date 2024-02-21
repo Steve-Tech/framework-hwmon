1d256dfdff587f46c5d51513b964afe619130c08
Stephen Horvath <s.horvath@outlook.com.au>
2024-02-21 11:29:04 +1000

    Small cleanup + add pwm max & min

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A Linux HWMON driver for Framework Laptops, may also work on other devices that 
 
 This was mostly just a proof of concept and a learning experience for me, so good luck if you want to use it.
 
-It currently it will read and controls the speed of up to 4 fans, but only the first fan has actually been tested.
+It currently it will read and control the speed of up to 4 fans, but only the first fan has actually been tested.
 
 As I have no idea what I'm doing, a lot of this code is based on the existing [framework-laptop-kmod](https://github.com/DHowett/framework-laptop-kmod) driver, I will hopefully be able to move my code to that project at some point.
 
@@ -12,7 +12,9 @@ As I have no idea what I'm doing, a lot of this code is based on the existing [f
 
 - `fan[1-4]_input` - Read fan speed in RPM (read-only)
 - `fan[1-4]_target` - Set target fan speed in RPM (write-only)
-- `pwm[1-4]` - Fan speed control in percent (write-only)
+- `pwm[1-4]` - Fan speed control in percent 0-100 (write-only)
 - `pwm[1-4]_enable` - Enable automatic fan control (write-only)
   - Currently you can write anything to enable, but I recommend writing `2` in case the driver is updated to support disabling automatic fan control.
   - Writing to the other interfaces will disable automatic fan control.
+- `pwm[1-4]_min` - returns 0 (read-only)
+- `pwm[1-4]_max` - returns 100 (read-only)
--- framework-hwmon.c
+++ framework-hwmon.c
@@ -26,8 +26,8 @@ static ssize_t ec_get_fan_speed(u8 idx, u16 *val)
 	return ec->cmd_readmem(ec, offset, sizeof(*val), val);
 }
 
-static ssize_t show_fan_speed(struct device *dev, struct device_attribute *attr,
-			      char *buf)
+static ssize_t fw_fan_speed_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
 {
 	struct sensor_device_attribute *sen_attr = to_sensor_dev_attr(attr);
 
@@ -37,7 +37,7 @@ static ssize_t show_fan_speed(struct device *dev, struct device_attribute *attr,
 	}
 
 	// Format as string for sysfs
-	return sprintf(buf, "%d\n", val);
+	return sysfs_emit(buf, "%u\n", val);
 }
 
 // --- fanN_target ---
@@ -75,8 +75,9 @@ static ssize_t ec_set_target_rpm(u8 idx, u32 *val)
 	return 0;
 }
 
-static ssize_t set_fan_target(struct device *dev, struct device_attribute *attr,
-			      const char *buf, size_t count)
+static ssize_t fw_fan_target_store(struct device *dev,
+				   struct device_attribute *attr,
+				   const char *buf, size_t count)
 {
 	struct sensor_device_attribute *sen_attr = to_sensor_dev_attr(attr);
 	u32 val;
@@ -127,8 +128,9 @@ static ssize_t ec_set_auto_fan_ctrl(u8 idx)
 	return 0;
 }
 
-static ssize_t set_pwm_enable(struct device *dev, struct device_attribute *attr,
-			      const char *buf, size_t count)
+static ssize_t fw_pwm_enable_store(struct device *dev,
+				   struct device_attribute *attr,
+				   const char *buf, size_t count)
 {
 	struct sensor_device_attribute *sen_attr = to_sensor_dev_attr(attr);
 
@@ -183,8 +185,8 @@ static ssize_t ec_set_fan_duty(u8 idx, u32 *val)
 	return 0;
 }
 
-static ssize_t set_pwm(struct device *dev, struct device_attribute *attr,
-		       const char *buf, size_t count)
+static ssize_t fw_pwm_store(struct device *dev, struct device_attribute *attr,
+			    const char *buf, size_t count)
 {
 	struct sensor_device_attribute *sen_attr = to_sensor_dev_attr(attr);
 	u32 val;
@@ -201,55 +203,82 @@ static ssize_t set_pwm(struct device *dev, struct device_attribute *attr,
 	return count;
 }
 
-#define FW_HWMON_RO_PERMS 0444
-#define FW_HWMON_WO_PERMS 0200
-#define FW_HWMON_RW_PERMS 0644
+static ssize_t fw_pwm_min_show(struct device *dev,
+			       struct device_attribute *attr, char *buf)
+{
+	return sysfs_emit(buf, "%i\n", 0);
+}
+
+static ssize_t fw_pwm_max_show(struct device *dev,
+			       struct device_attribute *attr, char *buf)
+{
+	return sysfs_emit(buf, "%i\n", 100);
+}
+
+#define FW_ATTRS_PER_FAN 6
 
 // clang-format off
-static SENSOR_DEVICE_ATTR(fan1_input, FW_HWMON_RO_PERMS, show_fan_speed, NULL, 0); // Fan Reading
-static SENSOR_DEVICE_ATTR(fan1_target, FW_HWMON_WO_PERMS, NULL, set_fan_target, 0); // Target RPM
-static SENSOR_DEVICE_ATTR(pwm1_enable, FW_HWMON_WO_PERMS, NULL, set_pwm_enable, 0); // Set Fan Control Mode
-static SENSOR_DEVICE_ATTR(pwm1, FW_HWMON_WO_PERMS, NULL, set_pwm, 0); // Set Fan Speed
+static SENSOR_DEVICE_ATTR_RO(fan1_input, fw_fan_speed, 0); // Fan Reading
+static SENSOR_DEVICE_ATTR_WO(fan1_target, fw_fan_target, 0); // Target RPM
+static SENSOR_DEVICE_ATTR_WO(pwm1_enable, fw_pwm_enable, 0); // Set Fan Control Mode
+static SENSOR_DEVICE_ATTR_WO(pwm1, fw_pwm, 0); // Set Fan Speed
+static SENSOR_DEVICE_ATTR_RO(pwm1_min, fw_pwm_min, 0); // Min Fan Speed
+static SENSOR_DEVICE_ATTR_RO(pwm1_max, fw_pwm_max, 0); // Max Fan Speed
 
-static SENSOR_DEVICE_ATTR(fan2_input, FW_HWMON_RO_PERMS, show_fan_speed, NULL, 1);
-static SENSOR_DEVICE_ATTR(fan2_target, FW_HWMON_WO_PERMS, NULL, set_fan_target, 1);
-static SENSOR_DEVICE_ATTR(pwm2_enable, FW_HWMON_WO_PERMS, NULL, set_pwm_enable, 1);
-static SENSOR_DEVICE_ATTR(pwm2, FW_HWMON_WO_PERMS, NULL, set_pwm, 1);
+static SENSOR_DEVICE_ATTR_RO(fan2_input, fw_fan_speed, 1);
+static SENSOR_DEVICE_ATTR_WO(fan2_target, fw_fan_target, 1);
+static SENSOR_DEVICE_ATTR_WO(pwm2_enable, fw_pwm_enable, 1);
+static SENSOR_DEVICE_ATTR_WO(pwm2, fw_pwm, 1);
+static SENSOR_DEVICE_ATTR_RO(pwm2_min, fw_pwm_min, 1);
+static SENSOR_DEVICE_ATTR_RO(pwm2_max, fw_pwm_max, 1);
 
-static SENSOR_DEVICE_ATTR(fan3_input, FW_HWMON_RO_PERMS, show_fan_speed, NULL, 2);
-static SENSOR_DEVICE_ATTR(fan3_target, FW_HWMON_WO_PERMS, NULL, set_fan_target, 2);
-static SENSOR_DEVICE_ATTR(pwm3_enable, FW_HWMON_WO_PERMS, NULL, set_pwm_enable, 2);
-static SENSOR_DEVICE_ATTR(pwm3, FW_HWMON_WO_PERMS, NULL, set_pwm, 2);
+static SENSOR_DEVICE_ATTR_RO(fan3_input, fw_fan_speed, 2);
+static SENSOR_DEVICE_ATTR_WO(fan3_target, fw_fan_target, 2);
+static SENSOR_DEVICE_ATTR_WO(pwm3_enable, fw_pwm_enable, 2);
+static SENSOR_DEVICE_ATTR_WO(pwm3, fw_pwm, 2);
+static SENSOR_DEVICE_ATTR_RO(pwm3_min, fw_pwm_min, 2);
+static SENSOR_DEVICE_ATTR_RO(pwm3_max, fw_pwm_max, 2);
 
-static SENSOR_DEVICE_ATTR(fan4_input, FW_HWMON_RO_PERMS, show_fan_speed, NULL, 3);
-static SENSOR_DEVICE_ATTR(fan4_target, FW_HWMON_WO_PERMS, NULL, set_fan_target, 3);
-static SENSOR_DEVICE_ATTR(pwm4_enable, FW_HWMON_WO_PERMS, NULL, set_pwm_enable, 3);
-static SENSOR_DEVICE_ATTR(pwm4, FW_HWMON_WO_PERMS, NULL, set_pwm, 3);
+static SENSOR_DEVICE_ATTR_RO(fan4_input, fw_fan_speed, 3);
+static SENSOR_DEVICE_ATTR_WO(fan4_target, fw_fan_target, 3);
+static SENSOR_DEVICE_ATTR_WO(pwm4_enable, fw_pwm_enable, 3);
+static SENSOR_DEVICE_ATTR_WO(pwm4, fw_pwm, 3);
+static SENSOR_DEVICE_ATTR_RO(pwm4_min, fw_pwm_min, 3);
+static SENSOR_DEVICE_ATTR_RO(pwm4_max, fw_pwm_max, 3);
 // clang-format on
 
-static struct attribute *fw_hwmon_attrs[(EC_FAN_SPEED_ENTRIES * 4) + 1] = {
-	&sensor_dev_attr_fan1_input.dev_attr.attr,
-	&sensor_dev_attr_fan1_target.dev_attr.attr,
-	&sensor_dev_attr_pwm1_enable.dev_attr.attr,
-	&sensor_dev_attr_pwm1.dev_attr.attr,
+static struct attribute
+	*fw_hwmon_attrs[(EC_FAN_SPEED_ENTRIES * FW_ATTRS_PER_FAN) + 1] = {
+		&sensor_dev_attr_fan1_input.dev_attr.attr,
+		&sensor_dev_attr_fan1_target.dev_attr.attr,
+		&sensor_dev_attr_pwm1_enable.dev_attr.attr,
+		&sensor_dev_attr_pwm1.dev_attr.attr,
+		&sensor_dev_attr_pwm1_min.dev_attr.attr,
+		&sensor_dev_attr_pwm1_max.dev_attr.attr,
 
-	&sensor_dev_attr_fan2_input.dev_attr.attr,
-	&sensor_dev_attr_fan2_target.dev_attr.attr,
-	&sensor_dev_attr_pwm2_enable.dev_attr.attr,
-	&sensor_dev_attr_pwm2.dev_attr.attr,
+		&sensor_dev_attr_fan2_input.dev_attr.attr,
+		&sensor_dev_attr_fan2_target.dev_attr.attr,
+		&sensor_dev_attr_pwm2_enable.dev_attr.attr,
+		&sensor_dev_attr_pwm2.dev_attr.attr,
+		&sensor_dev_attr_pwm2_min.dev_attr.attr,
+		&sensor_dev_attr_pwm2_max.dev_attr.attr,
 
-	&sensor_dev_attr_fan3_input.dev_attr.attr,
-	&sensor_dev_attr_fan3_target.dev_attr.attr,
-	&sensor_dev_attr_pwm3_enable.dev_attr.attr,
-	&sensor_dev_attr_pwm3.dev_attr.attr,
+		&sensor_dev_attr_fan3_input.dev_attr.attr,
+		&sensor_dev_attr_fan3_target.dev_attr.attr,
+		&sensor_dev_attr_pwm3_enable.dev_attr.attr,
+		&sensor_dev_attr_pwm3.dev_attr.attr,
+		&sensor_dev_attr_pwm3_min.dev_attr.attr,
+		&sensor_dev_attr_pwm3_max.dev_attr.attr,
 
-	&sensor_dev_attr_fan4_input.dev_attr.attr,
-	&sensor_dev_attr_fan4_target.dev_attr.attr,
-	&sensor_dev_attr_pwm4_enable.dev_attr.attr,
-	&sensor_dev_attr_pwm4.dev_attr.attr,
+		&sensor_dev_attr_fan4_input.dev_attr.attr,
+		&sensor_dev_attr_fan4_target.dev_attr.attr,
+		&sensor_dev_attr_pwm4_enable.dev_attr.attr,
+		&sensor_dev_attr_pwm4.dev_attr.attr,
+		&sensor_dev_attr_pwm4_min.dev_attr.attr,
+		&sensor_dev_attr_pwm4_max.dev_attr.attr,
 
-	NULL,
-};
+		NULL,
+	};
 
 static const struct attribute_group fw_hwmon_group = {
 	.attrs = fw_hwmon_attrs,
@@ -290,8 +319,8 @@ static int __init fw_hwmon_init(void)
 		ec_get_fan_speed(i, &val);
 		// EC returns -1 when the fan is not present
 		if (val == -1) {
-			// Remove the fan from the list, *4 because we have 4 attributes per fan
-			fw_hwmon_attrs[i * 4] = NULL;
+			// Remove the fan from the list
+			fw_hwmon_attrs[i * FW_ATTRS_PER_FAN] = NULL;
 			// the NULL terminates, so we can stop here
 			break;
 		}
